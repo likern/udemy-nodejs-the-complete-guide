a86450795cb5fb2e0bba18b8dad285d03114b9c8
Malov Victor <malvikus@gmail.com>
2019-01-21 22:03:22 +0300

    Fix name of html page

--- section6/app.js
+++ section6/app.js
@@ -15,9 +15,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((request, response) => {
-  response
-    .status(404)
-    .sendFile(path.join(__dirname, "views", "not-found.html"));
+  response.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
