2f02f2e7862e29a22cbeff6d67c6057a0a6fb88d
Malov Victor <malvikus@gmail.com>
2019-01-21 19:29:03 +0300

    1. Server css files statically 2. Add a bunch of html pages 3. Refactoring

--- section5/app.js
+++ section5/app.js
@@ -1,15 +1,23 @@
+const path = require("path");
 const express = require("express");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const rootDir = require("./util/path");
+
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In the middleware");
-  res.send('<h1>The "Add Product" Page</h1>');
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "public")));
 
-app.use("/", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("Hello from Express.js");
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((request, response) => {
+  response
+    .status(404)
+    .sendFile(path.join(__dirname, "views", "not-found.html"));
 });
 
 app.listen(3000);
