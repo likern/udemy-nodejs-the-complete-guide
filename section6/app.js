5066b5f367bf9b3e1dd6ad73261e1983efc0f8be
Malov Victor <malvikus@gmail.com>
2019-01-22 16:18:10 +0300

    Add Pug template engine support

--- section6/app.js
+++ section6/app.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const rootDir = require("./util/path");
 
@@ -8,14 +8,17 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((request, response) => {
-  response.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  response.status(404).render("404");
 });
 
 app.listen(3000);
