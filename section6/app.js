7432c249a34fd8071ec98d8ab73029c1980b5d88
Malov Victor <malvikus@gmail.com>
2019-01-22 22:48:26 +0300

    Adding EJS templating engine

--- section6/app.js
+++ section6/app.js
@@ -5,20 +5,10 @@ const shopRoutes = require("./routes/shop");
 const rootDir = require("./util/path");
 
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs"
-  })
-);
-
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
